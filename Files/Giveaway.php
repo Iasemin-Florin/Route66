4a6aba328089508ca16032afad4ccdbd3f857eb6
Iasemin-Florin <queue.triple@gmail.com>
2020-11-23 10:47:51 +0100

    Finished the Home Page and started on the Giveaway Page

---
+++ Files/Giveaway.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giveaway</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="../Images/logo.png" alt="logo" class="logo">
+            <nav>
+                <a href="#" class="hide-desktop">
+                    <i class="fa fa-bars" id="menu"></i>
+                </a>
+                <ul class="show-desktop hide-mobile" id="nav">
+                    <li id="exit" class="exit-btn hide-desktop">
+                        <img src="../Images/x.png" alt="" srcset="">
+                    </li>
+                    <li><a href="Index.php">Home</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="Giveaway.php">Giveaway</a></li>
+                </ul>
+            </nav>
+            <section>
+                <h1>The monthly Route66 Giveaway</h1>
+                <p>You will need to answer a quastion, which changes montly, and then you will need to fill out a form to enter.</p>
+            </section>
+        </header>
+    </div>
+
+</body>
+</html>
---
+++ Files/Index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..\CSS\Index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Main Page</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <img src="../Images/logo.png" alt="logo" class="logo">
+            <nav>
+                <a href="#" class="hide-desktop">
+                    <i class="fa fa-bars" id="menu"></i>
+                </a>
+                <ul class="show-desktop hide-mobile" id="nav">
+                    <li id="exit" class="exit-btn hide-desktop">
+                        <img src="../Images/x.png" alt="" srcset="">
+                    </li>
+                    <li><a href="Index.php">Home</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="Giveaway.php">Giveaway</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <img src="../Images/Route66Drink.png" alt="Energy drink" class="energy-drink">
+            <h1>Brand new Energy Drink to quench your thirst </h1>
+            <p class="subhead">Perfect for a long ride along route 66</p>
+        </section>
+    </div>
+    <div class="brown-container">
+        <div class="container">
+            <ul>
+                <li>
+                    <img src="../Images/calendar.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptates natus tempora corporis quidem! Iure nihil voluptatem itaque ex id minima quod! Iusto velit iure molestias vitae sit id est?</p>
+                </li>
+                <li>
+                    <img src="../Images/gift.png" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ea asperiores non mollitia beatae eaque maxime quasi tempore? Harum sequi natus earum, accusamus ipsam laboriosam at nam iusto sapiente dignissimos!</p>
+                </li>
+                <li>
+                    <img src="../Images/calendar.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi error accusantium vitae cupiditate cum, nemo at ex qui reiciendis asperiores dolorum! Iste excepturi quos ipsa mollitia facere quo vel commodi?</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-container">
+            <div class="container">
+                <a href="#">
+                    <img src="../Images/logo.png" alt="logo" class="logo">
+                </a>
+                <p>Welcome to the end of the Route <br> We hope you enjoyed the journey</p>
+            </div>
+        </div>
+    </footer>
+    <script src="Index.js"></script>
+</body>
+</html>
