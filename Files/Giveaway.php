d209070c4c125ff0f79fe20a398f347551ff7d85
Iasemin-Florin <queue.triple@gmail.com>
2020-11-25 08:32:11 +0100

    Added a database connection file, and a user registration

--- Files/Giveaway.php
+++ Files/Giveaway.php
@@ -1,33 +1,109 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Giveaway</title>
-</head>
-<body>
-    <div class="container">
-        <header>
-            <img src="../Images/logo.png" alt="logo" class="logo">
-            <nav>
-                <a href="#" class="hide-desktop">
-                    <i class="fa fa-bars" id="menu"></i>
-                </a>
-                <ul class="show-desktop hide-mobile" id="nav">
-                    <li id="exit" class="exit-btn hide-desktop">
-                        <img src="../Images/x.png" alt="" srcset="">
-                    </li>
-                    <li><a href="Index.php">Home</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="Giveaway.php">Giveaway</a></li>
-                </ul>
-            </nav>
-            <section>
-                <h1>The monthly Route66 Giveaway</h1>
-                <p>You will need to answer a quastion, which changes montly, and then you will need to fill out a form to enter.</p>
-            </section>
-        </header>
-    </div>
+<?php
+    include 'dbconnect.php';
+    
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $answer = $_POST['answer'];
+    
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
-</body>
-</html>
+    $currentTime = microtime(true);
+    $fnameERR = $lnameERR = $emailErr = $answerERR = "";
+    $sql = "";
+
+    $sql_e = "SELECT * FROM `userdata` WHERE mail = '$email'";
+    $res_e = mysqli_query($conn, $sql_e);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (empty($fname)) 
+        {
+            $fnameERR = "First name is required!";
+            echo $fnameERR;
+
+            return;
+        }
+        else 
+        {
+            $fname = test_input($_POST['fname']);
+
+            if (!preg_match("/^[a-zA-Z-']*$/", $fname)) 
+            {
+                $fnameERR = "Only letters and white space allowed";
+                echo $fnameERR;
+
+                return;
+            }
+        }
+        
+        if (empty($lname)) 
+        {
+            $lnameERR = "Last name is required!";
+            echo $lnameERR;
+
+            return;
+        }
+        else 
+        {
+            $lname = test_input($_POST['lname']);
+            
+            if (!preg_match("/^[a-zA-Z]*$/", $lname)) 
+            {
+                $lnameERR = "Only letters and white space allowed";
+                echo $lnameERR;
+
+                return;
+            }
+        }
+        if (empty($email)) 
+        {
+            $emailErr = "Email is required!";
+            echo $emailErr;
+
+            return;
+        }
+        else 
+        {
+            $email = test_input($_POST['email']);
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+            {
+                $emailErr = "Invalid email format";
+                echo $emailErr;
+
+                return;
+            }
+            else if (mysqli_num_rows($res_e) > 0)
+            {
+                $emailErr = "Sorry email is already taken";
+                echo $emailErr;
+                return;
+            }
+        }
+        if (empty($answer)) 
+        {
+            $answerERR = "An answer is required";
+            echo $answerERR;
+            return;
+        }
+        else 
+        {
+            //NOTE TIL IMORGEN, TJEK HVILKET ID DEN HAR, HVIS DEN HAR DEN RIGTIGT ID SÅ SKAL DEN RETURN TRUE, HVIS DEN HAR FALSE ID SÅ SKAL DEN RETURN FALSE I DATABASEN
+            if () {
+                
+            }
+            $answer = test_input($_POST['answer']);
+        }
+
+        $sql = "INSERT INTO `userdata` (`firstName`, `lastName`, `mail`, `entered`, `answer`) 
+        VALUES ('$fname', '$lname', '$email', '$currentTime', '$answer');";
+        mysqli_query($conn, $sql);
+        echo " error: ".mysqli_error($conn);
+    }
+?>
---
+++ Files/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+    $serverName = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "giveaway";
+
+    $conn = new mysqli($serverName, $username, $password, $dbname);
+    
+    if(!$conn){
+        die("Connection failed:". mysqli_connect_error());
+    }
+
+    /*CHECK IF DATABASE CONNECTION WORKS
+    else {
+        echo 'Congratz we connected bois ';
+    }*/
+?>
