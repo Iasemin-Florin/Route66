1fe25855678f719ede6258e789860d2d9dd22fc2
Iasemin-Florin <queue.triple@gmail.com>
2020-12-01 14:07:06 +0100

    have done some work on the administration table page

--- Files/Administration.php
+++ Files/Administration.php
@@ -22,12 +22,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../CSS/Administration.css">
-    <title>Document</title>
-
     <!-- Datatables CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
     <!-- Datatables Responsive CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+    <title>Document</title>
 </head>
 <body>
     <div class="container">
@@ -41,45 +40,45 @@
                     <li id="exit" class="exit-btn hide-desktop">
                         <img src="../Images/x.png" alt="" srcset="">
                     </li>
-                    <li><a href="Index.php">Home</a></li>
-                    <li><a href="Giveaway.html">Giveaway</a></li>
+                    <li><a href="http://localhost/Route99/Files/Index.php">Home</a></li>
+                    <li><a href="http://localhost/Route99/Files/Giveaway.html">Giveaway</a></li>
+                    <li><a href="http://localhost/Route99/Files/Questions.html">Questions</a></li>
                 </ul>
             </nav>
         </header>
     </div>
     <div class="brown-container">
-        <div class = "container">
-            <div class="data-tables">
-            <table id="dataTable" class="text-center dataTable no-footer">
-                <thead class="bg-light text-capitalize">
-                    <tr>
-                        <th>ID:</th>
-                        <th>First name:</th>
-                        <th>Last name:</th>
-                        <th>Email:</th>
-                        <th>Entered:</th>
-                        <th>Answer:</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                    foreach($data as $user)
-                    {
-                        echo "<th>".$user["userID"]."</th>
-                              <th>".$user["firstName"]."</th>
-                              <th>".$user["lastName"]."</th>
-                              <th>".$user["mail"]."</th>
-                              <th>".$user["entered"]."</th>
-                              <th>".$user["answer"]."</th>";
-                    }
+        <table>
+            <tr>
+                <th>ID:</th>
+                <th>First name:</th>
+                <th>Last name:</th>
+                <th>Email:</th>
+                <th>Entered:</th>
+                <th>Answer:</th>
+            </tr>
+            <tr>
+                <?php 
+                     foreach($data as $user)
+                     {
+                         echo "
+                            <tr>
+                                <td>".$user['userID']."</td>
+                                <td>".$user['firstName']."</td>
+                                <td>".$user['lastName']."</td>
+                                <td>".$user['mail']."</td>
+                                <td>".strtotime($user['entered'])."</td>
+                                <td>".$user['answer']."</td>
+                                <br>
+                            </tr>";
+                     }
                 ?>
-                </tbody>
-            </table>
-        </div>
+            </tr>
+        </table>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="Modal.js"></script>
     <script src="Index.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- Datatables JS -->
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
     <!-- Datatables Responsive JS -->
